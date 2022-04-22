d7a39a7e5da755b8c6eb3c345e407b4dfa0d2e22
Brenda <brendamvreal@gmail.com>
2022-04-22 19:34:32 -0400

    Remove semicolon on line 96, not necessary

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -91,7 +91,7 @@ const getAllReservations = function(guest_id, limit = 10) {
   WHERE reservations.guest_id = $1
   GROUP BY properties.id, reservations.id
   ORDER BY reservations.start_date
-  LIMIT $2;`, [guest_id, limit]);
+  LIMIT $2;`, [guest_id, limit])
   .then(result => {
     return result.rows;
   })
